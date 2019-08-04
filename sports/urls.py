9511736d97a9ec4512785ececddb8a921e2d428e
walkinthedawg <guitarfordawg@yahoo.com>
2019-08-04 00:19:42 +0000

    removed secret key and duplicate ADMIN declare in URL.PY

--- sports/urls.py
+++ sports/urls.py
@@ -18,7 +18,6 @@ from . import views
 from .views import get_sports, sport_detail, create_or_edit_sport, sports
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^$', sports, name='sports'),
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
